b0ce34afa31e142843946dbb2b763ee6044356a2
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2023-04-25 11:00:04 -0700

    Create convert.v
    add code for convert
    untested

---
+++ lab2/convert.v
@@ -0,0 +1,36 @@
+module convert (
+  // outputs
+  sign, abs,
+  // inputs
+  analog, clk
+  );
+  
+  output [11:0] abs; // MSB to LSB
+  output sign;
+  
+  input [11:0] analog;
+  input clk;
+  
+  int count;
+  
+  initial begin
+    count = 0;
+  end
+  
+  always @ (posedge clk) begin
+    if analog[11] == 0 begin
+      sign <= 0;
+      abs <= analog[11:0];
+    end
+    else begin
+      for (integer i = 0; i < 11; i += 1) begin
+        count += analog[i]
+      end
+      if (count == 0) begin // means we have -2048
+        abs = { analog[11:1], 1 };
+      end
+      else begin
+        abs = ~D + 1;
+      end
+    end
+  end
