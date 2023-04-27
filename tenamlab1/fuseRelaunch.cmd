2556e7bcf3ac043b7a0c90c948c5e8bf5f6f8427
Riley Bruins <ribru17@hotmail.com>
2023-04-27 11:04:57 -0700

    lab 2

--- tenamlab1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Student/Desktop/152alab10am/tenamlab1/tb_isim_beh.exe" -prj "C:/Users/Student/Desktop/152alab10am/tenamlab1/tb_beh.prj" "work.tb" "work.glbl" 
