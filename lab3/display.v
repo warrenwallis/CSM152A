cddd88f1e3497032e025a3c472d1c3a8a8911d48
Mike Zhan <61182063+mikezhan88@users.noreply.github.com>
2023-05-04 11:30:03 -0700

    Create display.v

---
+++ lab3/display.v
@@ -0,0 +1,19 @@
+module display (
+  //outputs
+  sec, min,
+  
+  //inputs
+  time
+);
+  
+  output integer sec;
+  output integer min;
+  
+  
+  input time;
+  
+  always @ (posedge clk) begin
+    
+  end
+  
+endmodule
