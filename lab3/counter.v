0c3c7734508815230de3513e86acf5f2b1e1d2ba
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2023-05-04 10:56:28 -0700

    Create counter.v

---
+++ lab3/counter.v
@@ -0,0 +1,43 @@
+module counter (
+  // outputs
+  sec, min,
+  // inputs
+  clk, sel, adj, rst, pse
+);
+  
+  output integer sec;
+  output integer min;
+  
+  input clk;
+  input sel;
+  input adj;
+  input rst;
+  input pse;
+  
+  always @ (posedge clk) begin
+    if (rst) begin // reset is pressed
+      sec = 0;
+      min = 0;
+    end
+    if (pse == 0) begin // pause is off
+      if (adj) begin // adjustment mode
+        sec = sec + 2;
+        if (sec >= 60) begin
+          sec = sec % 60 + 2; // sec could either be 0 or 1
+          min = min % 60 + 2; // min could overflow and either be an odd or even number
+        end
+      end
+      else begin // normal increment
+        sec = sec + 1;
+        if (sec == 60) begin
+          sec = 0;
+          min = min + 1;
+          if (min == 60) begin
+            min = 0;
+          end
+        end
+      end
+    end
+  end
+  
+endmodule
