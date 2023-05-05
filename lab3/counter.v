fb92f0332173ebb4b909b321f576cd9e7f74fb6a
warrenwallis <wpagsuguiron@g.ucla.edu>
2023-05-04 19:19:00 -0700

    fix output bug in counter

--- lab3/counter.v
+++ lab3/counter.v
@@ -2,7 +2,7 @@ module counter (
   // outputs
   sec, min,
   // inputs
-  clk, sel, adj, rst, pse
+  clk, sel, adj, rst, pse, tst
 );
   
   output integer sec;
@@ -13,8 +13,18 @@ module counter (
   input adj;
   input rst;
   input pse;
+  input tst;
+
+  integer step;
+
+  initial begin
+    sec = 0;
+    min = 0;
+    step = 0;
+  end
   
   always @ (posedge clk) begin
+    step = step + 1;
     if (rst) begin // reset is pressed
       sec = 0;
       min = 0;
@@ -24,26 +34,26 @@ module counter (
         if (sel) begin // second is selected
           sec = sec + 2;
           if (sec >= 60) begin
-            sec = sec % 60 + 2; // sec could either be 0 or 1
-            min = min % 60 + 2; // min could overflow and either be an odd or even number
+            sec = (sec + 2) % 60; // sec could either be 0 or 1
+            min = (min + 2) % 60; // min could overflow and either be an odd or even number
           end
         end
         else begin
-          min = min % 60 + 2; // min could overflow and either be an odd or even number
+          min = (min + 2) % 60; // min could overflow and either be an odd or even number
         end
       end
       else begin // normal increment
         sec = sec + 1;
         if (sec == 60) begin
           sec = 0;
-          min = min + 1;
-          if (min == 60) begin
-            min = 0;
-          end
+          min = (min + 1) % 60;
         end
       end
     end
-    $display("Stopwatch Output: %d:%d", min, sec);
+
+    if (tst) begin
+      $display("Stopwatch Output at step %d: %d:%d", step, min, sec);
+    end
   end
   
 endmodule
--- lab3/counter_TB.v
+++ lab3/counter_TB.v
@@ -2,20 +2,26 @@
 
 module counter_TB;
   
+  reg sec;
+  reg min;
   reg clk;
   reg sel;
   reg adj;
   reg rst;
   reg pse;
+  reg tst;
   
   initial begin
     clk = 0;
+    tst = 1;
     
     #5 send(0, 0, 0, 0);
-    #20 send(0, 1, 0, 1);
+    #20 send(0, 0, 0, 1);
     #30 send(0, 0, 0, 0);
-    #45 send (0, 0, 0, 1);
-    #60 send (0, 0, 0, 0);
+    #45 send(0, 1, 0, 0);
+    #60 send(0, 0, 0, 0);
+    #70 send(1, 1, 0, 0);
+    #80 send(0, 0, 0, 0);
     
     #1000;
     $finish;
@@ -24,11 +30,14 @@ module counter_TB;
   always #5 clk = ~clk;
   
   counter counter0_ (
+    .sec (sec),
+    .min (min),
     .clk (clk),
     .sel (sel),
     .adj (adj),
     .rst (rst),
-    .pse (pse)
+    .pse (pse),
+    .tst (tst)
   );
   
   task send;
