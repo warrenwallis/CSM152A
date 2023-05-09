699863a045d2c8a160555908e826e00ba3d895b3
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2023-05-09 11:08:48 -0700

    Create clock_TB.v

---
+++ lab3/clock_TB.v
@@ -0,0 +1,40 @@
+`timescale 1ns/ 1ps
+
+module clock_TB;
+  
+  reg one;
+  reg two;
+  reg faster;
+  reg clk;
+  reg tst;
+  
+  integer count;
+  
+  initial begin
+    clk = 0;
+    tst = 1;
+    count = 0;
+    
+    #1000;
+    $finish;
+  end
+  
+  always #5 clk = ~clk;
+  awlays #5 counter = counter + 1;
+  
+  clock clock0_ (
+    .one (one);
+    .two (two);
+    .faster (faster);
+    .tst (tst);
+  );
+  
+  task send
+    begin
+      one = o;
+      two = t;
+      faster = f;
+      $display("Time %d", count);
+    end
+    
+endmodule
