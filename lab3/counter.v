8b492cca9e593dcc2641ec758da05195afb5023f
Kai Jun Tay <tehohno@gmail.com>
2023-05-16 17:21:51 -0700

    Update counter.v
    changed counter to return each digit of the time instead of a 2-digits `sec` and a 2-digits `min`

--- lab3/counter.v
+++ lab3/counter.v
@@ -4,50 +4,83 @@ module counter (
     input sel,
     input pse,
     input rst,
-    output reg [5:0] sec,
-    output reg [5:0] min
+    output reg [3:0] sec_one,
+    output reg [3:0] sec_ten,
+    output reg [3:0] min_one,
+    output reg [3:0] min_ten
 );
-    initial sec = 6'b000000;
-    initial min = 6'b000000;
+    initial sec_one = 4'b0000;
+    initial sec_ten = 4'b0000;
+    initial min_one = 4'b0000;
+    initial min_ten = 4'b0000;
     
     always @ (posedge clk or posedge rst) begin
              
         if (rst == 1'b1) begin // reset is pressed
-            sec = 0;
-            min = 0;
+            sec_one = 4'b0000;
+            sec_ten = 4'b0000;
+            min_one = 4'b0000;
+            min_ten = 4'b0000;
         end
         
         else begin
             if (pse == 1'b0) begin
                 // ADJ is LOW, stopwatch behaves normally
                 if (adj == 1'b0) begin
-                    if (sec < 60) begin
-                        sec = sec + 1'b1;
+                    if (sec_one < 10) begin
+                        sec_one = sec_one + 1'b1;
                     end
                     else begin
-                        sec = 0;
-                        if (min < 60) begin
-                            min = min + 1'b1;
+                        sec_one = 4'b0000;
+                        if (sec_ten < 6) begin
+                            sec_ten = sec_ten + 1'b1;
                         end
                         else begin
-                            min = 0;
+                            sec_ten = 4'b0000;
+                            if(min_one < 10) begin
+                                min_one = min_one + 1'b1;
+                            end
+                            else begin
+                                min_one = 4'b0000;
+                                if (min_ten < 6) begin
+                                    min_ten = min_ten + 1'b1;
+                                end
+                                else begin
+                                    min_ten = 4'b0000;
+                                end
+                            end
                         end
                     end
                 end
                 // ADJ is HIGH, only increase selected digit
                 else begin
                     if (sel == 1'b0) begin
-                        // min selected
-                        min = min + 1;
-                        if (min >= 60) begin
-                            min = 0;
+                        if (min_one < 10) begin
+                            min_one = min_one + 1'b1;
+                        end
+                        else begin
+                            min_one = 4'b0000;
+                            if (min_ten < 6) begin
+                                min_ten = min_ten + 1'b1;
+                            end
+                            else begin
+                                min_ten = 4'b0000;
+                            end
                         end
                     end
                     else begin
                         // sec selected
-                        sec = sec + 1;
-                        if (sec >= 60) begin
-                            sec = 0;
+                        if (sec_one < 10) begin
+                            sec_one = sec_one + 1'b1;
+                        end
+                        else begin
+                            sec_one = 4'b0000;
+                            if (sec_ten < 6) begin
+                                sec_ten = sec_ten + 1'b1;
+                            end
+                            else begin
+                                sec_ten = 4'b0000;
+                            end
                         end
                     end
                 end
