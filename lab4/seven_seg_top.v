fa66b92416120e386eb82dd3daddfe21a6724f91
Kai Jun Tay <tehohno@gmail.com>
2023-06-08 11:14:51 -0700

    Update seven_seg_top.v

--- lab4/seven_seg_top.v
+++ lab4/seven_seg_top.v
@@ -6,11 +6,15 @@
 // - take in result (should be constantly being evaluated so that once button pressed, result will be shown)
 //
 module sevenseg_top(
-    input clk,
-    input btnS,
-    input btnR,
-    input btn1,
-    input [1:0] sw,
+    input hit,
+    input stay,
+    input rst,
+    input display_clk,
+    input point_display_sel,
+    input game_finished,
+    input result,
+    input [4:0] point,
+    input [15:0] cards,
     output reg [7:0] seg,
     output reg [3:0] an
     );
@@ -31,13 +35,6 @@ module sevenseg_top(
     localparam anode2 = 4'b1011;
     localparam anode3 = 4'b1101;
     localparam anode4 = 4'b1110;
-
-    wire hit, stay, rst, display_clk;
-    
-    debouncer debounce_hit(clk, btn1, hit);
-    debouncer debounce_stay(clk, btnS, stay);
-    debouncer debounce_rst(clk, btnR, rst);
-    clk_divider clock_divider(clk, rst, display_clk);
    
     ////////////////////////////////////////////////////////
     //
@@ -46,20 +43,13 @@ module sevenseg_top(
     ////////////////////////////////////////////////////////
     
     reg[15:0] cards = 16'b1111111111111111;
-    //temp card to simulate game
-    localparam card = 4'b1110; 
     
     wire [7:0] card1_seg, card2_seg, card3_seg, card4_seg;
         
     seven_seg card1(cards[15:12], card1_seg);
     seven_seg card2(cards[11:8], card2_seg);
     seven_seg card3(cards[7:4], card3_seg);
-    seven_seg card4(cards[3:0], card4_seg);
-    
-    always @ (posedge hit) begin
-        cards <= {cards[11:0], card}; // inserts the last card into the hand
-    end
-    
+    seven_seg card4(cards[3:0], card4_seg);   
     
     ////////////////////////////////////////////////////////
     //
@@ -67,8 +57,6 @@ module sevenseg_top(
     //
     ////////////////////////////////////////////////////////
     
-    // temp point of 21 to simulate game - to be replaced with input
-    reg [4:0] point = 5'b10101;
     reg [3:0] point_tens, point_ones;
     
     always @ * begin
@@ -86,14 +74,6 @@ module sevenseg_top(
     //  generating seven-seg signals for results
     //
     ////////////////////////////////////////////////////////
-    
-    reg game_state = 0;
-    // temp result - should be an input wire from the game module
-    wire result = sw[1];
-    
-    always @ (posedge stay) begin
-        game_state = ~game_state; // toggles the result display
-    end
     
     wire[7:0] lose_seg1, lose_seg2, lose_seg3, lose_seg4;
     wire[7:0] win_seg1, win_seg2, win_seg3, win_seg4;
@@ -118,7 +98,7 @@ module sevenseg_top(
     
     always @ (posedge display_clk) begin
     
-        if (game_state == 1) begin
+        if (game_finished == 1) begin
             if (anode == 0) begin
                 an <= anode1;
                 seg <= (result == 0) ? lose_seg1 : win_seg1;
@@ -143,7 +123,7 @@ module sevenseg_top(
         
         else begin
     
-            if (sw[0] == 1) begin // points display mode
+            if (point_display_sel == 1) begin // points display mode
                 if (anode == 0) begin
                     an <= anode1;
                     seg <= blank_seg;
