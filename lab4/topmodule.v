06d9f566aa8fab203df62adc68d181e3ca89ef2d
Riley Bruins <ribru17@hotmail.com>
2023-06-08 11:00:50 -0700

    fix: clean up topmodule

--- lab4/topmodule.v
+++ lab4/topmodule.v
@@ -2,8 +2,8 @@ module topmodule(
     input clk,
     input btnS,
     input btnR,
+    input btn1,
     input [7:0] sw,
-    output [7:0] led,
     output reg[7:0] seg,
     output reg[3:0] an
     );
@@ -12,33 +12,15 @@ module topmodule(
     wire clk_1khz;
     wire clk_25mhz; 
 
-    clock_divider clk_divider(clk, btnR, clk_200hz, clk_1khz, clk_25mhz);
+    
+    wire hit, stay, rst;
 
-    
-    reg [3:0] number = 4'b0001;
-    wire debounced_btns;
+    debouncer debouncebtnhit(clk, btnS, hit);
+    debouncer debouncebtnstay(clk, btnR, stay);
+    debouncer debouncebtnrst(clk, btn1, rst);
 
-    debouncer debouncebtns(clk, btnS, debounced_btns);
+    clock_divider clk_divider(clk, rst, clk_200hz, clk_1khz, clk_25mhz);
     
-    always @ (posedge debounced_btns) begin
-        if (number < 4) begin
-            number = number + 1;
-        end
-        else begin
-            number = 1;
-        end
-    end
+    game game_mod(clk_200hz, clk_1khz, clk_25mhz, hit, stay);
     
-    wire [7:0] number_segment;
-    
-    seven_seg test(number, number_segment);
-    
-    always @ (posedge clk_1khz) begin
-        an <= 4'b1110;
-        seg <= number_segment;
-    end
-    
-    
-    assign led = sw;
-
 endmodule
